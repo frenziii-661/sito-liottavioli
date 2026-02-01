4d85a1672808a99a307b79b691a3faba6dcec90f
Francesco Liotta Violi <f.liottavioli@MacBook-Air-di-Francesco.local>
2026-02-01 16:40:42 +0100

    Forzo l'invio dei file generati di Tina

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,78 @@
+// tina/config.ts
+import { defineConfig } from "tinacms";
+var config_default = defineConfig({
+  branch: "main",
+  clientId: null,
+  // Lascia null per il lavoro in locale
+  token: null,
+  // Lascia null per il lavoro in locale
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public"
+    }
+  },
+  schema: {
+    collections: [
+      {
+        name: "page",
+        label: "Pagine",
+        path: "content/pages",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "titolo_grande",
+            label: "Titolo Principale"
+          },
+          {
+            type: "string",
+            name: "sottotitolo",
+            label: "Sottotitolo/Bio",
+            ui: {
+              component: "textarea"
+            }
+          }
+        ]
+      },
+      {
+        name: "post",
+        label: "News / Articoli",
+        path: "content/posts",
+        format: "md",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Titolo News",
+            isTitle: true,
+            required: true
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Data"
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Immagine Copertina"
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Testo Articolo",
+            isBody: true
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
